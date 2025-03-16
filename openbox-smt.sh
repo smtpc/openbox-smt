d571f48e3c5497318a506533f867013a3c6ff2e7
smtpc <smtpc2024@gmail.com>
2025-03-16 20:37:30 -0300

    refactor autostart file

--- openbox-smt.sh
+++ openbox-smt.sh
@@ -2,7 +2,8 @@
 
 echo "[INSTALANDO PROGRAMAS]"
 sudo apt update
-sudo apt install xinit xterm xorg openbox guake compton conky feh fish pcmanfm tint2 dmenu firefox-esr htop git -y
+sudo apt install xinit xterm xorg openbox guake compton conky feh fish dmenu htop git -y
+#pcmanfm tint2
 
 echo "[startx]"
 echo -e "exec openbox-session" >> ~/.xinitrc
@@ -12,5 +13,5 @@ sudo cp conky.conf /etc/conky/conky.conf
 
 echo "[openbox autosart]"
 wget -P ~/Downloads https://www.kali.org/wallpapers/images/2025/kali-maze.jpg
-echo -e "compton &\nconky -c /etc/conky/conky.conf &\nfeh --bg-center ~/Downloads/kali-maze.jpg &\nguake" > ~/.config/autostart
+echo -e "compton &\nconky -c /etc/conky/conky.conf &\nfeh --bg-center ~/Downloads/kali-maze.jpg &\nguake" > ~/.config/openbox/autostart
 
